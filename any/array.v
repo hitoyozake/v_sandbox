204efc2eb1245f01eb71d977351fc4fe02ad13e9
hitoyozake <mikoto_1122@yahoo.co.jp>
2019-07-11 12:48:38 +0900

    learn_array

---
+++ any/array.v
@@ -0,0 +1,32 @@
+
+
+fn main(){
+  // 親スコープを汚すシャドーイングは不可
+
+    // := ・・・宣言  = ・・・代入
+    a := 2
+    println( a)
+    /*
+    if true {
+        a := 3
+    } 
+   */ 
+
+   mut b := 100
+   b = 120
+   println(b)
+
+   mut names := ['Jon', 'Tom']
+
+   names << 'Taro'
+   
+   println(names.len)
+   println(names.contains('Jack'))
+   println('Jon' in names)
+
+   for name in names{
+        println(name)
+    }
+
+}
+
